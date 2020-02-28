dc1ea8ecba6d39d4a027d39ff518b4baad5bd8a2
jdab2020 <jdabalos90@gmail.com>
2020-02-27 23:24:45 -0800

    added commentary | two query functions

--- db/query.js
+++ db/query.js
@@ -16,7 +16,7 @@ class DB {
     constructor(connection) {
         this.connection = connection;
     }
-
+    // display all employees and used for selecting id for modifying database
     findAllEmployees() {
         return this.connection.query(
             "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary FROM employee " +
@@ -25,13 +25,13 @@ class DB {
         )
 
     }
-
+    // selects departments table to be used to select department.id
     findAllDepartments() {
         return this.connection.query(
             "SELECT * FROM department"
         )
     }
-
+    // display employees of a selected department
     findAllEmployeesByDepartment(departmentId) {
         return this.connection.query(
             "SELECT employee.id, employee.first_name, employee.last_name, role.title FROM employee " +
@@ -40,7 +40,7 @@ class DB {
             "WHERE department.id = ?", departmentId
         );
     }
-
+    // display employees under a selected manager
     findAllEmployeesByManager(managerId) {
         return this.connection.query(
             "SELECT employee.id, employee.first_name, employee.last_name, department.name AS department, role.title FROM employee " +
@@ -49,27 +49,44 @@ class DB {
             "WHERE manager_id = ?", managerId
         )
     }
-
+    // removes a selected employee
     removeEmployee(employeeId) {
         return this.connection.query(
             "DELETE FROM employee " +
             "WHERE employee.id = ?", employeeId
         )
     }
-
+    // selects role table to be used to select role.id
     findAllRoles() {
         return this.connection.query(
             "SELECT * FROM role"
         )
     }
-
+    // updates the role of selected employee by id with selected role by role.id
     updateEmployeeRole(employeeId, roleId) {
         return this.connection.query(
             "UPDATE employee " +
             "SET role_id = ? " +
-            "WHERE employee.id = ?", [employeeId, roleId]
+            "WHERE employee.id = ?", [roleId, employeeId]
         )
     }
+    // selects all but the chosen employee that is being updated
+    findAllPossibleManagers(employeeId) {
+        return this.connection.query(
+            "SELECT * FROM employee " +
+            "WHERE id != ?", employeeId
+        )
+    }
+    // updates selected employee's manager
+    updateEmployeeManager(employeeId, managerId) {
+        return this.connection.query(
+            "UPDATE employee " +
+            "SET manager_id = ? " +
+            "WHERE employee.id = ?", [managerId, employeeId]
+        )
+    }
+
+
 
 
 }
